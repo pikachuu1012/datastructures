fa72b669113e609df7a1175c8eeacc200274cd55
Christina Ong <christinaong1012@gmail.com>
2023-05-09 21:56:37 -0700

    finished, need andrew to talk through stuff

--- villager_data.py
+++ villager_data.py
@@ -1,5 +1,18 @@
 """Functions to parse a file containing villager data."""
 
+#opens, splits according to | 
+##############################
+        # f = open("villagers.csv")
+        # for line in f:
+        #     villager_entry = line.split("|")
+        #     print(villager_entry)
+        #     print()
+
+        # f.close()
+#############################
+
+
+
 
 def all_species(filename):
     """Return a set of unique species in the given file.
@@ -10,12 +23,20 @@ def all_species(filename):
     Return:
         - set[str]: a set of strings
     """
+    f = open(filename)
+    species_list = []
 
-    species = set()
+    for line in f:
+        entry = line.split("|")
+        species_list.append(entry[1])
+
+    species = set(species_list)
+    f.close()
 
     # TODO: replace this with your code
-
+    print(species)
     return species
+#all_species("villagers.csv")
 
 
 def get_villagers_by_species(filename, search_string="All"):
@@ -32,10 +53,18 @@ def get_villagers_by_species(filename, search_string="All"):
     villagers = []
 
     # TODO: replace this with your code
-
+    f = open(filename)
+   
+    for line in f: 
+        villager_entry = line.split("|")
+        if search_string == villager_entry[1]:
+            villagers.append(villager_entry[0])
+    print(sorted(villagers))
     return sorted(villagers)
 
+#get_villagers_by_species("villagers.csv", "Cat")
 
+#       ?????? ****** IS THIS WHAT THEY MEAN ???? ####
 def all_names_by_hobby(filename):
     """Return a list of lists containing villagers' names, grouped by hobby.
 
@@ -47,9 +76,20 @@ def all_names_by_hobby(filename):
     """
 
     # TODO: replace this with your code
+    f = open(filename)
+    listoflists = []
+    for entry in f:
+        villager = entry.split("|")
+        listoflists.append(villager)
+    
 
+    listoflists.sort(key = lambda listoflists: listoflists[3]) #sorts by hobby
+    for l in listoflists:
+        print(l)
+
+    f.close()
     return []
-
+all_names_by_hobby("villagers.csv")
 
 def all_data(filename):
     """Return all the data in a file.
@@ -67,9 +107,17 @@ def all_data(filename):
     all_data = []
 
     # TODO: replace this with your code
-
+    f = open(filename)
+    for line in f:
+        entry = line.split("|")
+        #name, species, personality, hobby, catchphrase
+        mytuple = (entry[0], entry[1], entry[2], entry[3], entry[4])
+        
+        all_data.append(mytuple)
+    f.close()
+    print(all_data)
     return all_data
-
+all_data("villagers.csv")
 
 def find_motto(filename, villager_name):
     """Return the villager's motto.
@@ -86,7 +134,19 @@ def find_motto(filename, villager_name):
     """
 
     # TODO: replace this with your code
-
+    f = open(filename)
+    for line in f:
+        villager_entry = line.split("|")
+        name = villager_entry[0]
+        if name == villager_name:
+            print(villager_entry[-1])
+            return villager_entry[-1]
+    f.close()
+    print("No villager with name: ", villager_name)
+    return None
+find_motto("villagers.csv", "Tangy")
+find_motto("villagers.csv", "Tagy")
+find_motto("villagers.csv", "Marshal")
 
 def find_likeminded_villagers(filename, villager_name):
     """Return a set of villagers with the same personality as the given villager.
@@ -104,3 +164,29 @@ def find_likeminded_villagers(filename, villager_name):
     """
 
     # TODO: replace this with your code
+    
+
+    f = open(filename)
+    personality = "blank"
+    myset = {1,}
+    for line in f:
+        entry = line.split("|")
+        name = entry[0]
+        if name == villager_name:
+            personality = entry[2]
+            break
+        
+    print("personality to look for: ", personality)
+    for line in f: 
+        
+        entry = line.split("|")
+
+        if personality == entry[2]:
+            myset.add(entry[0])
+
+    myset.remove(1)
+    print(myset)
+    return myset
+
+
+find_likeminded_villagers("villagers.csv", "Tangy")
